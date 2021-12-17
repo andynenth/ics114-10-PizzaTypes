0ab9be6fea2df472f2e74db423627b41c3b3d418
Andy Nenthong <C0516154@intra.camosun.bc.ca>
2021-12-17 13:11:32 -0800

    Add project files.

---
+++ PizzaTypes/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace PizzaTypes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoLargeSize = new System.Windows.Forms.RadioButton();
+            this.rdoMediumSize = new System.Windows.Forms.RadioButton();
+            this.rdoSmallSize = new System.Windows.Forms.RadioButton();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbMushroom = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbPepperoni = new System.Windows.Forms.CheckBox();
+            this.cbBlackOlive = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtOrder = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoLargeSize);
+            this.groupBox1.Controls.Add(this.rdoMediumSize);
+            this.groupBox1.Controls.Add(this.rdoSmallSize);
+            this.groupBox1.Location = new System.Drawing.Point(92, 24);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(148, 132);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pizza size";
+            // 
+            // rdoLargeSize
+            // 
+            this.rdoLargeSize.AutoSize = true;
+            this.rdoLargeSize.Location = new System.Drawing.Point(29, 92);
+            this.rdoLargeSize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.rdoLargeSize.Name = "rdoLargeSize";
+            this.rdoLargeSize.Size = new System.Drawing.Size(51, 19);
+            this.rdoLargeSize.TabIndex = 2;
+            this.rdoLargeSize.Text = "large";
+            this.rdoLargeSize.UseVisualStyleBackColor = true;
+            this.rdoLargeSize.CheckedChanged += new System.EventHandler(this.PizzaSize_CheckedChanged);
+            // 
+            // rdoMediumSize
+            // 
+            this.rdoMediumSize.AutoSize = true;
+            this.rdoMediumSize.Location = new System.Drawing.Point(29, 65);
+            this.rdoMediumSize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.rdoMediumSize.Name = "rdoMediumSize";
+            this.rdoMediumSize.Size = new System.Drawing.Size(70, 19);
+            this.rdoMediumSize.TabIndex = 1;
+            this.rdoMediumSize.Text = "medium";
+            this.rdoMediumSize.UseVisualStyleBackColor = true;
+            this.rdoMediumSize.CheckedChanged += new System.EventHandler(this.PizzaSize_CheckedChanged);
+            // 
+            // rdoSmallSize
+            // 
+            this.rdoSmallSize.AutoSize = true;
+            this.rdoSmallSize.Location = new System.Drawing.Point(29, 37);
+            this.rdoSmallSize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.rdoSmallSize.Name = "rdoSmallSize";
+            this.rdoSmallSize.Size = new System.Drawing.Size(53, 19);
+            this.rdoSmallSize.TabIndex = 0;
+            this.rdoSmallSize.Text = "small";
+            this.rdoSmallSize.UseVisualStyleBackColor = true;
+            this.rdoSmallSize.CheckedChanged += new System.EventHandler(this.PizzaSize_CheckedChanged);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(112, 304);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(116, 23);
+            this.txtTotal.TabIndex = 1;
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 307);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Total:";
+            // 
+            // cbMushroom
+            // 
+            this.cbMushroom.AutoSize = true;
+            this.cbMushroom.Location = new System.Drawing.Point(26, 30);
+            this.cbMushroom.Name = "cbMushroom";
+            this.cbMushroom.Size = new System.Drawing.Size(116, 19);
+            this.cbMushroom.TabIndex = 3;
+            this.cbMushroom.Text = "Mushroom (+$2)";
+            this.cbMushroom.UseVisualStyleBackColor = true;
+            this.cbMushroom.CheckedChanged += new System.EventHandler(this.PizzaSize_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbPepperoni);
+            this.groupBox2.Controls.Add(this.cbBlackOlive);
+            this.groupBox2.Controls.Add(this.cbMushroom);
+            this.groupBox2.Location = new System.Drawing.Point(92, 163);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(148, 125);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Toppings";
+            // 
+            // cbPepperoni
+            // 
+            this.cbPepperoni.AutoSize = true;
+            this.cbPepperoni.Location = new System.Drawing.Point(26, 89);
+            this.cbPepperoni.Name = "cbPepperoni";
+            this.cbPepperoni.Size = new System.Drawing.Size(111, 19);
+            this.cbPepperoni.TabIndex = 5;
+            this.cbPepperoni.Text = "Pepperoni (+$5)";
+            this.cbPepperoni.UseVisualStyleBackColor = true;
+            this.cbPepperoni.CheckedChanged += new System.EventHandler(this.PizzaSize_CheckedChanged);
+            // 
+            // cbBlackOlive
+            // 
+            this.cbBlackOlive.AutoSize = true;
+            this.cbBlackOlive.Location = new System.Drawing.Point(26, 58);
+            this.cbBlackOlive.Name = "cbBlackOlive";
+            this.cbBlackOlive.Size = new System.Drawing.Size(113, 19);
+            this.cbBlackOlive.TabIndex = 4;
+            this.cbBlackOlive.Text = "Black olive (+$3)";
+            this.cbBlackOlive.UseVisualStyleBackColor = true;
+            this.cbBlackOlive.CheckedChanged += new System.EventHandler(this.PizzaSize_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "Cheese",
+            "Hawaiian",
+            "Margherita",
+            "Neapolitan",
+            "Lahma Bi Afeen",
+            "Calzone",
+            "Stromboli",
+            "Deep disk",
+            "M&L Special"});
+            this.listBox1.Location = new System.Drawing.Point(318, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(143, 124);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txtOrder
+            // 
+            this.txtOrder.Location = new System.Drawing.Point(318, 178);
+            this.txtOrder.Multiline = true;
+            this.txtOrder.Name = "txtOrder";
+            this.txtOrder.Size = new System.Drawing.Size(143, 110);
+            this.txtOrder.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 363);
+            this.Controls.Add(this.txtOrder);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "M&L Pizza Palace";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoLargeSize;
+        private System.Windows.Forms.RadioButton rdoMediumSize;
+        private System.Windows.Forms.RadioButton rdoSmallSize;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbMushroom;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cbPepperoni;
+        private System.Windows.Forms.CheckBox cbBlackOlive;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtOrder;
+    }
+}
---
+++ PizzaTypes/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaTypes
+{
+    public partial class Form1 : Form
+    {
+        float pizzaPrice;
+        float totalPrice;
+        string selectedPizza;
+        public Form1()
+        {
+            InitializeComponent();
+            pizzaPrice = 0f;
+            totalPrice = 0f;
+        }
+
+        private void PizzaSize_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CalulatePizzaCost()
+        {
+
+            switch (selectedPizza)
+            {
+                case "Cheese": 
+                    pizzaPrice = 10;
+                    break;
+                case "Neapolitan":
+                    pizzaPrice = 10;
+                    break;
+                case "Margherita":
+                    pizzaPrice = 10;
+                    break;
+                case "Calzone":
+                    pizzaPrice = 12.5f;
+                    break;
+                case "Stromboli":
+                    pizzaPrice = 12.5f;
+                    break;
+                case "Deep dish":
+                    pizzaPrice = 12.5f;
+                    break;
+                case "Marinara":
+                    pizzaPrice = 12.5f;
+                    break;
+                case "Hawaiian":
+                    pizzaPrice = 12.5f;
+                    break;
+            }
+
+            totalPrice = pizzaPrice;
+            if (cbMushroom.Checked)
+            {
+                totalPrice += 2;
+            }
+            if (cbBlackOlive.Checked)
+            {
+                totalPrice += 3;
+            }
+            if (cbPepperoni.Checked)
+            {
+                totalPrice += 5;
+            }
+
+                txtTotal.Text = String.Format("{0:C}", totalPrice);
+        }
+
+        private bool IsAtLeastOneSizeChecked()
+        {
+            return rdoSmallSize.Checked || rdoMediumSize.Checked || rdoLargeSize.Checked;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedPizza = listBox1.SelectedItem.ToString();
+            CalulatePizzaCost();
+        }
+    }
+}
